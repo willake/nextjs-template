4220a7e473dee82a0b07823d0713797a214b7577
Hui En <Willake>
2022-02-01 13:06:17 +0800

    refactor meta component to familiar styels

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,39 +1,26 @@
-import { Component } from 'react';
 import Head from 'next/head';
 
-interface Props {
+type Props = {
     title: string,
     keywords: string,
     description: string
 }
 
-type MetaProps = typeof Meta.defaultProps & Props;
-
-class Meta extends Component<MetaProps> {
-    static defaultProps = {
-        title: 'Nextjs Template',
-        keywords: 'fill your keywords here',
-        descrition: ''
-    };
-
-    constructor(props: MetaProps) {
-        super(props);
-    }
-    
-    render() {
-        return (
-            <Head>
-                <meta name='viewport'
-                content='width=device-width,
-                initial-scale=1' />
-                <meta name='keywords' content={this.props.keywords} />
-                <meta name='description' content = {this.props.description} />
-                <meta charSet='utf-8' />
-                <link rel="icon" href="/favicon.ico" />
-                <title>{this.props.title}</title>
-            </Head>
-        )
-    }
-}
+const Meta: React.FC<Props> = ({ 
+    title = 'Nextjs Template', 
+    keywords = 'fill your keywords here', 
+    description = '' }) => {
+    return (
+        <Head>
+            <meta name='viewport'
+            content='width=device-width, initial-scale=1' />
+            <meta name='keywords' content={keywords} />
+            <meta name='description' content = {description} />
+            <meta charSet='utf-8' />
+            <link rel="icon" href="/favicon.ico" />
+            <title>{title}</title>
+        </Head>
+    );
+};
 
 export default Meta;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,25 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Meta from '../components/meta'
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Hello World <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
-    </div>
+    <>
+      <Meta 
+        title='Home Page'
+        keywords=''
+        description=''
+      />
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Hello World <a href="https://nextjs.org">Next.js!</a>
+          </h1>
+        </main>
+      </div>
+    </>
   )
 }
 
